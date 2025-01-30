b8d24797b4fd8cfcdd1634020319839baf78b054
Polina <polinawakeup@yandex.ru>
2025-01-31 00:15:52 +0300

    Реализована страница входа и регистрации

---
+++ public/loginPage.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let userForm = new UserForm();
+
+function loginCallback(response){
+    console.log(response);
+
+    if(response.success){
+        location.reload();
+    } else {
+        userForm.setLoginErrorMessage(response.error);
+    };
+}
+
+userForm.loginFormCallback = function(data){
+    ApiConnector.login(data, loginCallback);
+};
+
+function registerCallback(response){
+    console.log(response);
+    if(response.success){
+        location.reload();
+    } else {
+        userForm.setRegisterErrorMessage(response.error);
+    };
+}
+
+userForm.registerFormCallback = function(data){
+    ApiConnector.register(data, registerCallback);
+}
